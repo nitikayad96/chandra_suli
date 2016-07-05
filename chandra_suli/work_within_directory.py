2a57ec06fa5e2bc64013e6312c21d895423dbd81
giacomov <giacomo.vianello@gmail.com>
2016-07-05 12:58:34 -0700

    A context manager to work in a directory

---
+++ chandra_suli/work_within_directory.py
@@ -0,0 +1,22 @@
+import contextlib
+import os
+
+
+@contextlib.contextmanager
+def work_within_directory(directory):
+
+    original_directory = os.getcwd()
+
+    os.chdir(directory)
+
+    try:
+
+        yield
+
+    except:
+
+        raise
+
+    finally:
+
+        os.chdir(original_directory)
