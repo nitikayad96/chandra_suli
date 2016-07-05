4a5e72de89f2d1f4c4b134bd8a6a77b05898485e
giacomov <giacomo.vianello@gmail.com>
2016-07-05 12:58:21 -0700

    Initial setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, Extension
+
+
+setup(
+    name="chandra_suli",
+
+    packages=['chandra_suli'],
+
+    # The __version__ comes from the exec at the top
+
+    version='0.0.1',
+
+    description="SULI project",
+
+    author='Nitika Yadlapalli',
+
+    author_email='nitikayad96@gmail.com',
+
+    url='https://github.com/nitikayad96/chandra_suli',
+
+    keywords=['SULI', 'Chandra', 'transients'],
+
+    classifiers=[],
+
+    install_requires=[],
+
+)
+
