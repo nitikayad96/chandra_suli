b750dac2976ab5c957cf398bff9f37961914e050
giacomov <giacomo.vianello@gmail.com>
2016-07-05 12:58:44 -0700

    Substitute for the linux find command

---
+++ chandra_suli/find_files.py
@@ -0,0 +1,16 @@
+import fnmatch
+import os
+
+
+def find_files(directory, pattern):
+
+    matches = []
+
+    for root, dirnames, filenames in os.walk(directory):
+
+        for filename in fnmatch.filter(filenames, pattern):
+
+            matches.append(os.path.abspath(os.path.join(root, filename)))
+
+    return matches
+
