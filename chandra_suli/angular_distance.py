25498018a16ddc570b3d91d77b69c5476b29c5c6
giacomov <giacomo.vianello@gmail.com>
2016-07-06 15:26:15 -0700

    Now it's possible to customize the output unit

--- chandra_suli/angular_distance.py
+++ chandra_suli/angular_distance.py
@@ -2,7 +2,7 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 
 
-def angular_distance(ra1, dec1, ra2, dec2):
+def angular_distance(ra1, dec1, ra2, dec2, unit='degree'):
     """
     Compute angular distance between pairs of coordinates
 
@@ -10,12 +10,13 @@ def angular_distance(ra1, dec1, ra2, dec2):
     :param dec1:
     :param ra2:
     :param dec2:
+    :param unit: the unit of the output (default: degree)
     :return: angular distance
     """
     point_1 = SkyCoord(ra=ra1 * u.degree, dec=dec1 * u.degree, frame='icrs')
 
     point_2 = SkyCoord(ra=ra2 * u.degree, dec=dec2 * u.degree, frame='icrs')
 
-    angular_distance = point_1.separation(point_2).to(u.degree)
+    angular_distance = point_1.separation(point_2).to(unit)
 
     return angular_distance.value
