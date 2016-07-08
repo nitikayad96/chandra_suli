e9b897d79591f7301f694d587f0d9b3e6f6b412c
Fermi User <nitikayad96@gmail.com>
2016-07-08 14:55:46 -0400

    Download files and save names

---
+++ chandra_suli/chandra_pipeline.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Run all steps of pipeline with one command
+"""
+
+import argparse
+import subprocess
+import os
+
+from chandra_suli import find_files
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser(description='Download event files and exposure map from the Chandra catalog')
+
+    parser.add_argument("--obsid",help="Observation ID Numbers", type=int, required=True)
+
+    args = parser.parse_args()
+
+    cmd_line = "download_by_obsid.py --obsid %d" %args.obsid
+
+    subprocess.check_call(cmd_line,shell=True)
+
+    evtfile = os.path.basename(find_files.find_files(os.getcwd(),'*evt2.fits.gz')[0])
+    tsvfile = os.path.basename(find_files.find_files(os.getcwd(),"%d.tsv" %args.obsid)[0])
+    expfile = os.path.basename(find_files.find_files(os.getcwd(),"*exp3.fits.gz")[0])
+
+    cmd_line = ""
