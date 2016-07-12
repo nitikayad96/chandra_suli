57886bf65141da2035a5cfcf0a2f062e13af6768
giacomov <giacomo.vianello@gmail.com>
2016-07-12 14:58:28 -0700

    Represents the Chandra PSF

---
+++ chandra_suli/chandra_psf.py
@@ -0,0 +1,25 @@
+import psf
+import caldb4
+
+
+class ChandraPSF(object):
+
+    def __init__(self):
+
+        cdb = caldb4.Caldb(telescope="CHANDRA", product="REEF")
+        reef = cdb.search[0]
+        extno = cdb.extno()
+
+        # Replace the trailing '[..]' block number specifier
+        reef = reef.split('[')[0] + "[{}]".format(extno + 1)
+
+        self._pdata = psf.psfInit(reef)
+
+    def get_psf_size(self, angle_in_arcmin, energy_in_kev=1.5, percent_level=0.9, phi=0.0):
+
+        # Get the size of the psf at the minimum and maximum theta
+        # for an energy of 1.5 keV
+
+        psf_size = psf.psfSize(self._pdata, energy_in_kev, angle_in_arcmin, phi, percent_level)
+
+        return psf_size
