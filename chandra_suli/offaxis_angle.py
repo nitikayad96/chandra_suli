aa388e9d3d1fee06472c0d3bb33d593691ae3874
giacomov <giacomo.vianello@gmail.com>
2016-07-12 14:58:41 -0700

    Provides a function to compute the off-axis angle of a given sky position

---
+++ chandra_suli/offaxis_angle.py
@@ -0,0 +1,35 @@
+# Chandra. Automatically compute the step size, as the average
+# size of the PSF in the detector
+
+from astropy.coordinates import SkyCoord
+import astropy.io.fits as pyfits
+import astropy.units as u
+
+
+def get_offaxis_angle(ra, dec, event_file):
+
+    # Get the aim point
+
+    with pyfits.open(event_file) as f:
+
+        ra_pointing = f['EVENTS'].header.get("RA_PNT")
+        dec_pointing = f['EVENTS'].header.get("DEC_PNT")
+        system = f['EVENTS'].header.get("RADECSYS")
+
+    if system is None:
+
+        system = 'ICRS'
+
+    # Compute the corresponding off-axis angle theta
+
+    pointing = SkyCoord(ra=ra_pointing * u.degree, dec=dec_pointing * u.degree, frame=system.lower())
+
+    c1 = SkyCoord(ra=ra * u.degree, dec=dec * u.degree, frame=system.lower())
+
+    this_theta = c1.separation(pointing)
+
+    # Store it in arcmin
+
+    theta = this_theta.to(u.arcmin).value
+
+    return theta
