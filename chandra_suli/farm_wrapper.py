ff55f12fba905a36fe795d6ad2e40d573308ba80
giacomov <giacomo.vianello@gmail.com>
2016-07-22 11:46:56 -0700

    Correcting problem when importing data (stage in)

--- chandra_suli/farm_wrapper.py
+++ chandra_suli/farm_wrapper.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
             # Remove from the directory name the obsid, so that /dev/shm/1241/regions/616 becomes
             # /dev/shm/1241/regions
 
-            local_reg_repo = os.path.sep.join(os.path.split(data_dir)[:-1])
+            local_reg_repo = os.path.sep.join(os.path.split(local_reg_dir)[:-1])
 
             cmd_line = "farm_step2.py --obsid %s --region_repo %s --adj_factor %s " \
                        "--emin %s --emax %s --ncpus %s --typeIerror %s --sigmaThreshold %s " \
