c1c8cd888a48819c6c2a0170a9d4ff6edbbbfdcf
Fermi User <nitikayad96@gmail.com>
2016-08-05 14:45:24 -0400

    read from NFS

--- chandra_suli/farm_wrapper.py
+++ chandra_suli/farm_wrapper.py
@@ -172,21 +172,21 @@ if __name__ == "__main__":
                 ######
                 # Copy region repository
                 ######
-
-                os.makedirs('regions')
-
-                reg_dir = os.path.join(regdir, str(this_obsid))
-
-                local_reg_dir = copy_directory(reg_dir, 'regions')
+                #
+                # os.makedirs('regions')
+                #
+                # reg_dir = os.path.join(regdir, str(this_obsid))
+                #
+                # local_reg_dir = copy_directory(reg_dir, 'regions')
 
                 # Remove from the directory name the obsid, so that /dev/shm/1241/regions/616 becomes
                 # /dev/shm/1241/regions
 
-                local_reg_repo = os.path.sep.join(os.path.split(local_reg_dir)[:-1])
+                # local_reg_repo = os.path.sep.join(os.path.split(local_reg_dir)[:-1])
 
                 cmd_line = "farm_step2.py --obsid %s --region_repo %s --adj_factor %s " \
                            "--emin %s --emax %s --ncpus %s --typeIerror %s --sigmaThreshold %s " \
-                           "--multiplicity %s --verbosity %s" % (this_obsid, local_reg_repo, args.adj_factor,
+                           "--multiplicity %s --verbosity %s" % (this_obsid, args.regdir, args.adj_factor,
                                                                  args.emin, args.emax, args.ncpus,
                                                                  args.typeIerror, args.sigmaThreshold, args.multiplicity,
                                                                  args.verbosity)
