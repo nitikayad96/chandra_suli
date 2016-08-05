518b5c1a9815935058f856a0d9d2f6f2dffef2c1
giacomov <giacomo.vianello@gmail.com>
2016-08-05 14:30:27 -0700

    Setup the environment for non-interactive use of FTOOLS

---
+++ chandra_suli/setup_ftools.py
@@ -0,0 +1,15 @@
+import os
+
+
+def setup_ftools_non_interactive():
+    """
+    Setup the environment so the FTOOLS can be run in the batch farm without a terminal attached to them.
+
+    :return: none
+    """
+
+    os.environ['HEADASNOQUERY'] = ''
+    os.environ['HEADASPROMPT'] = ''
+
+
+
