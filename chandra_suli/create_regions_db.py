f10c1c63589788a37295b56bd1d1c9c46c1aa155
giacomov <giacomo.vianello@gmail.com>
2016-08-04 18:10:32 -0700

    Create and query the database of all the sources

---
+++ chandra_suli/create_regions_db.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""
+Create the database of the regions, containing the R.A,Dec of each source and the path to its region file
+"""
+
+import argparse
+import os
+import sys
+import re
+import astropy.io.fits as pyfits
+import fitsio
+import collections
+
+from chandra_suli import find_files
+from chandra_suli import work_within_directory
+from chandra_suli.logging_system import get_logger
+from chandra_suli.sanitize_filename import sanitize_filename
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser(description='Create source database')
+
+    parser.add_argument('--region_dir', help="Directory containing the regions file for this obsid",
+                        type=str, required=True)
+
+    parser.add_argument('--outfile', help="Name of the output (filtered) event file", type=str, required=True)
+
+    parser.add_argument("--debug", help="Debug mode (yes or no)", type=bool, required=False, default=False)
+
+    # assumption = all level 3 region files and event file are already downloaded into same directory, the region_dir
+
+    args = parser.parse_args()
+
+    region_dir = sanitize_filename(args.region_dir)
+
+    log = get_logger("create_regions_db")
+
+    with work_within_directory.work_within_directory(region_dir):
+
+        # Find all region files
+        region_files = find_files.find_files('.', '*_reg3.fits.gz')
+
+        log.info("Found %s region files" % len(region_files))
+
+        db = collections.OrderedDict()
+
+        for i, region_file in enumerate(region_files):
+
+            sys.stderr.write("\r%s out of %s" % (i+1, len(region_files)))
+
+            header = fitsio.read_header(region_file, "SRCREG")
+
+            ra = header['RA']
+            dec = header['DEC']
+            obsid = header['OBS_ID']
+
+            # with pyfits.open(region_file) as f:
+            #
+            #     ra = f['SRCREG'].header.get("RA")
+            #     dec = f['SRCREG'].header.get("DEC")
+            #     obsid = f['SRCREG'].header.get("OBS_ID")
+
+            assert ra is not None, "Cannot find R.A. in file %s" % region_file
+            assert dec is not None, "Cannot find Dec. in file %s" % region_file
+            assert obsid is not None, "Cannot find OBSID in file %s" % region_file
+
+            try:
+
+                ra = float(ra)
+                dec = float(dec)
+
+            except:
+
+                raise RuntimeError("Cannot convert coordinates (%s,%s) to floats" % (ra, dec))
+
+            try:
+
+                obsid = int(obsid)
+
+            except:
+
+                raise RuntimeError("Cannot convert obsid %s to integer" % obsid)
+
+            # Get the source name from the path
+
+            try:
+
+                g = re.match('(.+)/(CXOJ.+)/.+', region_file)
+
+                name = g.groups()[1]
+
+            except:
+
+                raise RuntimeError("Cannot figure out the name of the source from the path %s" % (region_file))
+
+            db[name] = (ra, dec, obsid, os.path.relpath(region_file))
+
+    sys.stderr.write("done\n")
+
+    # Write the outfile
+
+    outfile = sanitize_filename(args.outfile)
+
+    with open(outfile, 'w+') as f:
+
+        f.write("#NAME RA DEC OBSID REGION_FILE\n")
+
+        for source_name in db:
+
+            ra, dec, obsid, region_file = db[source_name]
+
+            f.write("%s %.5f %.5f %i %s\n" % (source_name, ra, dec, obsid, region_file))
---
+++ chandra_suli/query_region_db.py
@@ -0,0 +1,32 @@
+import numpy as np
+import os
+
+from chandra_suli.angular_distance import angular_distance
+
+
+def query_region_db(ra_center, dec_center, radius, region_dir):
+    """
+    Returns a list of files relative to regions which are within the provided cone
+
+    :param ra_center: R.A. of the center of the cone
+    :param dec_center: Dec. of the center of the cone
+    :param radius: Radius of the cone (in arcmin)
+    :param region_dir: Path of the directory containing the database file and the region files
+    :return: list of region files
+    """
+
+    database_file = os.path.join(region_dir, 'region_database.txt')
+
+    data = np.recfromtxt(database_file, names=True)
+
+    # Compute the angular distance between all regions and the center of the cone
+
+    distances = angular_distance(float(ra_center), float(dec_center), data["RA"], data["DEC"], unit='arcmin')
+
+    # Select all regions within the cone
+
+    idx = (distances <= float(radius))
+
+    # Return the corresponding region files
+
+    return data['REGION_FILE'][idx]
