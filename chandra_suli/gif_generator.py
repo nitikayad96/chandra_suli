b02120ee4abdb39fa40b9149ff5e1206945bdb25
Fermi User <nitikayad96@gmail.com>
2016-10-18 15:04:06 -0400

    Full script to animate images - generating empty gifs?

--- chandra_suli/gif_generator.py
+++ chandra_suli/gif_generator.py
@@ -11,8 +11,11 @@ import numpy as np
 import astropy.io.fits as pyfits
 import matplotlib.pyplot as plt
 import seaborn as sbs
+from matplotlib.colors import LogNorm
+from astropy.convolution import convolve, Gaussian2DKernel
+from matplotlib.animation import ArtistAnimation
 
-from chandra_suli import find_files
+from chandra_suli.find_files import find_files
 from chandra_suli import logging_system
 from chandra_suli.run_command import CommandRunner
 from chandra_suli.sanitize_filename import sanitize_filename
@@ -49,7 +52,7 @@ if __name__=="__main__":
 
         duration = tstop-tstart
 
-        event_file = find_files.find_files(os.path.join(data_path, str(obsid)), "ccd_%s_%s_filtered.fits" % (ccd, obsid))[0]
+        event_file = find_files(os.path.join(data_path, str(obsid)), "ccd_%s_%s_filtered.fits" % (ccd, obsid))[0]
 
         intervals = [tstart, tstop]
 
@@ -83,5 +86,54 @@ if __name__=="__main__":
                        % (event_file, intervals[i], intervals[i+1], outfile)
 
             runner.run(cmd_line)
+
+        #sort all time interval fits files
+        images = find_files('.', "%s_TI*cand_%s%s" %(evt_names[0], candidate, evt_names[1]))
+        img_sort = sorted(images)
+
+        #create a list of frames that will be animated into a gif
+        frames = []
+
+        for image in img_sort:
+
+            #get x and y coordinates of image from fits file
+            data = pyfits.getdata(image)
+            x = data.field("x")
+            y = data.field("y")
+
+            #create 2D histogram data from it
+            sbs.set(rc={'axes.facecolor': 'black', 'axes.grid': False})
+            img = plt.hist2d(x, y, bins=60, norm=LogNorm())
+
+            #get binned coordinates
+            X = img[1]
+            Y = img[2]
+
+            #smooth data
+            gauss_kernel = Gaussian2DKernel(0.75)
+            smoothed_data_gauss = convolve(img[0], gauss_kernel)
+
+            #format it for hot colors
+            plt.hot()
+            plt.colorbar()
+
+            fig, axes = plt.subplots(nrows=1, ncols=1)
+
+            img2 = axes.matshow(smoothed_data_gauss.T, origin='lower')
+
+            axes.set_title("ObsID %s, CCD %s \nTstart = %s, Tstop = %s" %(obsid, ccd, tstart, tstop))
+            axes.set_yticklabels([])
+            axes.set_xticklabels([])
+
+            #add this image to list of frames
+            frames.append([img2])
+
+        #create new figure to animate gif
+        fig2 = plt.figure()
+
+        #animate and save gif
+        print "Creating gif ObsID %s, CCD %s, Candidate %s..." %(obsid, ccd, candidate)
+        anim = ArtistAnimation(fig2, frames, interval=200)
+        anim.save("%s_cand_%s.gif" %(evt_names[0], candidate))
 
 
